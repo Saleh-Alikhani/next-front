a747e631221ac1b8b62dacf63657fe4b7871e857
Saleh Alikhani <salehalikhani@gmail.com>
2023-04-05 00:18:06 +0330

    update logged page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 
 import Router from 'next/router'
-import { useUser } from '@auth0/nextjs-auth0/client'
+
 
 
 export default function Home() {
-  const {isLoading,user} = useUser()
   return (
     <>
-    123<button onClick={()=>Router.push('/api/auth/login')}>login</button>{user?.name}
-    123<button onClick={()=>Router.push('/api/auth/logout')}>logout</button>{user?.nickname}
+    123<button onClick={()=>Router.push('/api/auth/login')}>login</button>
+    123<button onClick={()=>Router.push('/api/auth/logout')}>logout</button>
     </>
   )
 }
--- src/pages/logged.tsx
+++ src/pages/logged.tsx
@@ -1,9 +1,12 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
 import React from 'react'
 
-const logged = () => {
-  return (
-    <div>logged,test</div>
+const Logged:React.FC = () => {
+  const {isLoading,user} = useUser()
+
+  return isLoading?<>wait..</>:(
+    <div>logged,test{user?.nickname}</div>
   )
 }
 
-export default logged
+export default Logged;
