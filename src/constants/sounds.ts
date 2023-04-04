9e91ec41b947f0484290f7002a72d0f0728670ce
Saleh Alikhani <salehalikhani@gmail.com>
2023-04-05 02:52:12 +0330

    vital commit

---
+++ src/constants/sounds.ts
@@ -0,0 +1,6 @@
+export const CAT_SOUNDS = [
+  'https://bigsoundbank.com/UPLOAD/wav/0390.wav',
+  'https://bigsoundbank.com/UPLOAD/wav/0903.wav',
+  'https://bigsoundbank.com/UPLOAD/wav/1890.wav',
+  'https://bigsoundbank.com/UPLOAD/wav/1901.wav',
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-
-
-import Router from 'next/router'
-
-
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Router from 'next/router';
 
 export default function Home() {
+  const { isLoading, user, error } = useUser();
 
-  return (
+  return isLoading || error ? (
+    <>{error?.message}</>
+  ) : (
     <>
-    123<button onClick={()=>Router.push('/api/auth/login')}>login</button>
-    123<button onClick={()=>Router.push('/logged')}>logged</button>
+      123<button onClick={() => Router.push('/api/auth/login')}>login</button>
+      123<button onClick={() => Router.push('/logged')}>logged</button>
+      {user && <>welcome {user.name}</>}
     </>
-  )
+  );
 }
