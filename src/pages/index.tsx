0e8dced574d28a971a8093e709bd3796ee2139ba
Saleh Alikhani <salehalikhani@gmail.com>
2023-04-05 01:04:28 +0330

    fix mistake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Router from 'next/router'
 
 export default function Home() {
   const {isLoading,user,error }= useUser()
-  return isLoading||error?<></>:(
+  return isLoading||error?<>{error}</>:(
     <>
     123<button onClick={()=>Router.push('/api/auth/login')}>login{user?.name}{user?.nickname}{user?.email}{user?.picture}</button>
     123<button onClick={()=>Router.push('/api/auth/logout')}>logout</button>
